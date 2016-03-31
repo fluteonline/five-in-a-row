aaeeb38e4a76af279198a85a698b1d1e016b28fb
luk <lovemomoco@gmail.com>
2016-03-31 14:51:37 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,64 @@
-/**
- * Created by luk on 2016/3/31.
- */
+var chessBoard = [],
+    me = true;
+
+for(var i=0;i<15;i++){
+    chessBoard[i] = [];
+    for(var j=0;j<15;j++){
+        chessBoard[i][j] = 0;
+    }
+}
+
+var chess = document.getElementById('chess'),
+    context = chess.getContext('2d');
+
+context.strokeStyle = '#bfbfbf';
+
+var bg = new Image();
+bg.src = 'images/bg.jpg';
+bg.onload = function(){
+    context.drawImage(bg,0,0,450,450);
+    drawChessBoard();
+};
+
+var drawChessBoard = function(){
+    for(var i=0;i<15;i++){
+        context.moveTo(15+i*30,15);
+        context.lineTo(15+i*30,435);
+        context.stroke();
+        context.moveTo(15,15+i*30);
+        context.lineTo(435,15+i*30);
+        context.stroke();
+    }
+};
+
+var oneStep = function(i,j,me){
+    context.beginPath();
+    context.arc(15+i*30,15+j*30,13,0,2*Math.PI);
+    context.closePath();
+    var gradient = context.createRadialGradient(15+i*30+2,15+j*30-2,13,15+i*30+2,15+j*30-2,0);
+    if(me){
+        gradient.addColorStop(0,'#0a0a0a');
+        gradient.addColorStop(1,'#636766');
+    }else{
+        gradient.addColorStop(0,'#d1d1d1');
+        gradient.addColorStop(1,'#f9f9f9');
+    }
+    context.fillStyle = gradient;
+    context.fill();
+};
+
+chess.onclick = function(e){
+    var x = e.offsetX,
+        y = e.offsetY,
+        i = Math.floor(x/30),
+        j = Math.floor(y/30);
+    if(chessBoard[i][j] == 0){
+        oneStep(i,j,me);
+        if(me){
+            chessBoard[i][j] = 1;
+        }else{
+            chessBoard[i][j] = 2;
+        }
+        me = !me;
+    }
+};
