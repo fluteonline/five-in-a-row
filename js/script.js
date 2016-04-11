946e3158462d2a40545d3d76fe5521f01bf1c2be
luk <lovemomoco@gmail.com>
2016-04-11 18:42:40 +0800

    update

--- js/script.js
+++ js/script.js
@@ -105,29 +105,114 @@ chess.onclick = function(e){
     if(over){
         return;
     }
+    if(!me){
+        return;
+    }
     var x = e.offsetX,
         y = e.offsetY,
         i = Math.floor(x/30),
         j = Math.floor(y/30);
     if(chessBoard[i][j] == 0){
         oneStep(i,j,me);
-        if(me){
-            chessBoard[i][j] = 1;
-        }else{
-            chessBoard[i][j] = 2;
-        }
+        chessBoard[i][j] = 1;
+
         //radar(i,j);
-        me = !me;
+
+        //判断胜负
         for(var k=0;k<count;k++){
             if(wins[i][j][k]){
                 myWin[k]++;
                 computerWin[k] = 6;
                 if(myWin[k]==5){
-                    alert('you win');
+                    alert('You Win');
                     over = true;
                 }
             }
         }
+        if(!over){
+            me = !me;
+            setTimeout(computerAI,600);
+        }
+    }
+};
+
+var computerAI = function(){
+    var myScore = [],
+        computerScore = [],
+        max = 0;
+    var u = 0,
+        v = 0;
+    for(var i=0;i<15;i++){
+        myScore[i] = [];
+        computerScore[i] = [];
+        for(var j=0;j<15;j++){
+            myScore[i][j] = 0;
+            computerScore[i][j] = 0;
+        }
+    }
+    for(i=0;i<15;i++){
+        for(j=0;j<15;j++){
+            if(chessBoard[i][j]==0){
+                for(var k=0;k<count;k++){
+                    if(wins[i][j][k]){
+                        if(myWin[k]==1){
+                            myScore[i][j] += 200;
+                        }else if(myWin[k]==2){
+                            myScore[i][j] += 400;
+                        }else if(myWin[k]==3){
+                            myScore[i][j] += 2000;
+                        }else if(myWin[k]==4){
+                            myScore[i][j] += 10000;
+                        }
+                        if(computerWin[k]==1){
+                            computerScore[i][j] += 220;
+                        }else if(computerWin[k]==2){
+                            computerScore[i][j] += 420;
+                        }else if(computerWin[k]==3){
+                            computerScore[i][j] += 2100;
+                        }else if(computerWin[k]==4){
+                            computerScore[i][j] += 20000;
+                        }
+                    }
+                }
+                if(myScore[i][j]>max){
+                    max = myScore[i][j];
+                    u = i;
+                    v = j;
+                }else if(myScore[i][j]==max){
+                    if(computerScore[i][j]>computerScore[u][v]){
+                        u = i;
+                        v = j;
+                    }
+                }
+                if(computerScore[i][j]>max){
+                    max = computerScore[i][j];
+                    u = i;
+                    v = j;
+                }else if(computerScore[i][j]==max){
+                    if(myScore[i][j]>myScore[u][v]){
+                        u = i;
+                        v = j;
+                    }
+                }
+            }
+        }
+    }
+    oneStep(u,v,false);
+    chessBoard[u][v] = 2;
+    //判断胜负
+    for(k=0;k<count;k++){
+        if(wins[u][v][k]){
+            computerWin[k]++;
+            myWin[k] = 6;
+            if(computerWin[k]==5){
+                alert('Com Win');
+                over = true;
+            }
+        }
+    }
+    if(!over) {
+        me = !me;
     }
 };
 
